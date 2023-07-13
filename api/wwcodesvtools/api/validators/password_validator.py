db0fef0bd3b90fdc4d1bf1d058440c684a09d7fa
Viridiana Ponce Angulo <102187795+viriponce@users.noreply.github.com>
2023-07-13 12:54:04 -0700

    BE- Fix validate password (#770)
    * Update the serializer used when user completes the registration to validate the password and the files related with the validation process.
    
    * Deleted comment
    
    ---------
    
    Co-authored-by: Viridiana Ponce Angulo <102187795+viri-ponce@users.noreply.github.com>

--- api/wwcodesvtools/api/validators/password_validator.py
+++ api/wwcodesvtools/api/validators/password_validator.py
@@ -13,12 +13,12 @@ def validate_password(value):
     pw_min = 8
     pw_max = 50
 
+    if (pw_min > len(value) or len(value) > pw_max):
+        raise ValidationError("Password should be a minimum of 8 and maximum of 50 characters long")
     if not re.search(r'[A-Z]', value):
         raise ValidationError("Password should have at least one uppercase letter")
     if not re.search(r'[a-z]', value):
         raise ValidationError("Password should have at least one lowercase letter")
     if not re.search(r'\d', value):
         raise ValidationError("Password should have at least one number")
-    if (pw_min > len(value) or len(value) > pw_max):
-        raise ValidationError("Password should be a minimum of 8 and maximum of 50 characters long")
     return value
