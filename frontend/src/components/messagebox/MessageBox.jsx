7a4610832f66d2608f72b250c69dc93d16fe2623
hissy55 <67436481+hissy55@users.noreply.github.com>
2023-08-21 21:30:07 -0700

    Fe sprint9 new registration link (#779)
    * in progress
    
    * Refactor code a bit for Register component.
    
    * Fix tests
    
    * Create a new component for the form

--- frontend/src/components/messagebox/MessageBox.jsx
+++ frontend/src/components/messagebox/MessageBox.jsx
@@ -3,29 +3,31 @@ import React from "react";
 import styles from "./MessageBox.module.css";
 import cx from "classnames";
 
-const MessageBox = (props) => {
-  const type = {
+const MessageBox = ({type, title, message, children}) => {
+  const types = {
     Info: "Info",
     Success: "Success",
     Error: "Error",
   };
 
   const getTypeStr = (str) => {
-    return type[str] || this.type.Info;
+    return types[str] || this.types.Info;
   };
 
   return (
     <div
       className={cx(
-        styles[getTypeStr(props.type).toLowerCase()],
+        styles[getTypeStr(type).toLowerCase()],
         styles["message-box"],
         "d-flex align-items-center justify-content-center flex-column"
       )}
       data-testid="message-box"
     >
-      <div className={styles["title"]}>{props.title}</div>
+      <div className={styles["title"]}>{title}</div>
 
-      <div className={styles["message"]}>{props.message}</div>
+      <div className={styles["message"]}>{message}</div>
+
+     {children}
     </div>
   );
 };
